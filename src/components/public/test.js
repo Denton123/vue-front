6c85d3def4a8c65ca0b5aed48aebcb6c43094dd1
Denton123 <1198503549@qq.com>
2018-10-17 17:58:39 +0800

    删除测试文件

--- src/components/public/test.js
+++
@@ -1,5 +0,0 @@
-export var a = 1
-export var s = 3
-
-var o = 234
-export default o
--- src/components/public/test3.js
+++
@@ -1,8 +0,0 @@
-module.exports = null
-module.exports = {
-	foo: 'hh'
-}
-module.exports = function two (argument) {
-	// body... 
-	return 2
-}
